209de4dc04358f9d6bddb367396a16f1c7c3092c
osmar <osmarbraz@hotmail.com>
2021-05-09 08:53:00 -0300

    Comentários e formatação.

--- app/src/main/java/com/exemplo/listview_meses/MainActivity.java
+++ app/src/main/java/com/exemplo/listview_meses/MainActivity.java
@@ -23,32 +23,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Associa os componentes da interface aos componentes da classe
+        // Associa os componentes da interface aos componentes da classe
         listView = findViewById(R.id.listView);
-        //Vetor de string com os valores para o Listview
+        // Vetor de string com os valores para o Listview
         String[] valores = new String[]{"Janeiro", "Fevereiro", "Março",
                 "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
                 "Outubro", "Novembro", "Dezembro"};
 
-        //Cria um ArrayList do vetor de String
+        // Cria um ArrayList do vetor de String
         ArrayList<String> listaValores = new ArrayList<>(Arrays.asList(valores));
-        //Especialista em lidar com Adapter e listas de valores
+        // Especialista em lidar com Adapter e listas de valores
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaValores);
-        //Associa o adapter a lista de visualização
+        // Associa o adapter a lista de visualização
         listView.setAdapter(adapter);
-        //Associa evento aos itens da lista
+        // Associa evento aos itens da lista
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Cria uma caixa de diálogo para exibir informações do item
+                // Cria uma caixa de diálogo para exibir informações do item
                 AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
-                //Título da janela
+                // Título da janela
                 dialogo.setTitle("Alerta!");
-                //Mensagem com o índice do item selecionado
+                // Mensagem com o índice do item selecionado
                 dialogo.setMessage("Você selecionou " + parent.getItemAtPosition(position)+ " na posição "+ position);
-                //Botão de ok do diálogo
+                // Botão de ok do diálogo
                 dialogo.setNeutralButton("Ok", null);
-                //Mostra a janela de diálogo
+                // Mostra a janela de diálogo
                 dialogo.show();
             }
         });
@@ -64,15 +64,15 @@ public class MainActivity extends AppCompatActivity {
         dialogo.setMessage("Você tem certeza que deseja sair?"); //Mensagem da janela de diálogo
         dialogo.setPositiveButton("Sim", new DialogInterface.OnClickListener() { //Evento para o botão sim
             public void onClick(DialogInterface dialog, int which) {
-                //Ação para a resposta sim
+                // Ação para a resposta sim
                 Toast.makeText(MainActivity.this, "Fechando o aplicativo", Toast.LENGTH_SHORT).show();
                 System.exit(0);
             }
         });
         dialogo.setNegativeButton("Não", new DialogInterface.OnClickListener() { //Evento para o botão não
             public void onClick(DialogInterface dialog, int which) {
-                //Ação para a resposta não
-                //Apenas fecha a mensagem
+                // Ação para a resposta não
+                // Apenas fecha a mensagem
                 dialog.dismiss();
             }
         });
