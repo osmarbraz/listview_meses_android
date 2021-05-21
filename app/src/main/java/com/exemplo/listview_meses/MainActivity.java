c846e2ad342cc2ce3d77c2601911a2e7321c1211
osmar <osmarbraz@hotmail.com>
2021-05-21 14:35:49 -0300

    Ajuste do onclick!

--- app/src/main/java/com/exemplo/listview_meses/MainActivity.java
+++ app/src/main/java/com/exemplo/listview_meses/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Associa os componentes da interface aos componentes da classe
         listView = findViewById(R.id.listView);
+
         // Vetor de string com os valores para o Listview
         String[] valores = new String[]{"Janeiro", "Fevereiro", "Março",
                 "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
@@ -33,25 +34,38 @@ public class MainActivity extends AppCompatActivity {
         // Cria um ArrayList do vetor de String
         ArrayList<String> listaValores = new ArrayList<>(Arrays.asList(valores));
         // Especialista em lidar com Adapter e listas de valores
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaValores);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaValores);
         // Associa o adapter a lista de visualização
         listView.setAdapter(adapter);
+
         // Associa evento aos itens da lista
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Cria uma caixa de diálogo para exibir informações do item
-                AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
-                // Título da janela
-                dialogo.setTitle("Alerta!");
-                // Mensagem com o índice do item selecionado
-                dialogo.setMessage("Você selecionou " + parent.getItemAtPosition(position)+ " na posição "+ position);
-                // Botão de ok do diálogo
-                dialogo.setNeutralButton("Ok", null);
-                // Mostra a janela de diálogo
-                dialogo.show();
+            public void onItemClick(AdapterView<?> parent, View v, int posicao, long id) {
+                // Chama o método que ira executar o evento na lista
+                onClickListView(parent, v, posicao, id);
             }
         });
+    }
+
+    /**
+     * Evento para o click no listview
+     * @param parent janela pai
+     * @param v
+     * @param posicao Índice do elemento selecionado no listview
+     * @param id
+     */
+    public void onClickListView(AdapterView<?> parent, View v, int posicao, long id){
+        // Cria uma caixa de diálogo para exibir informações do item
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
+        // Título da janela
+        dialogo.setTitle("Alerta!");
+        // Mensagem com o índice do item selecionado
+        dialogo.setMessage("Você selecionou " + parent.getItemAtPosition(posicao)+ " na posição "+ posicao);
+        // Botão de ok do diálogo
+        dialogo.setNeutralButton("Ok", null);
+        // Mostra a janela de diálogo
+        dialogo.show();
     }
 
     /**
